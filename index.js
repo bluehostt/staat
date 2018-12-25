c66b34387917719941e79e5d251ee42cdfc82d65
bluehostt <44468166+bluehostt@users.noreply.github.com>
2018-12-26 00:08:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,112 @@
-const Discord = require('discord.js');
+
+
+
+const Discord = require("discord.js");
+
+const RichEmbed = require("discord.js");
+
+const { Client, Util } = require('discord.js');
+
 const client = new Discord.Client();
 
+const prefix = "z";
 
-const developers = ["333140974865874944"]
+ 
+
+const devs = ["333140974865874944"]
+
+ 
+
 const adminprefix = "z";
+
 client.on('message', message => {
-    var argresult = message.content.split(`).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
+
+    var argresult = message.content.split(` `).slice(1).join(' ');
+
+      if (!devs.includes(message.author.id)) return;
+
+     
+
   if (message.content.startsWith(adminprefix + 'ply')) {
+
     client.user.setGame(argresult);
-      message.channel.send(Now Playig   ${argresult})
-  } else 
-     if (message.content === (adminprefix + "leaveserver")) {
+
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+
+  } else
+
+    if (message.content === (adminprefix + "Percie")) {
+
     message.guild.leave();        
+
   } else  
-  if (message.content.startsWith(adminprefix + 'wat')) {
+
+  if (message.content.startsWith(adminprefix + 'wt')) {
+
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(Now Watching   ${argresult})
-  } else 
-  if (message.content.startsWith(adminprefix + 'lis')) {
+
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+
+  } else
+
+  if (message.content.startsWith(adminprefix + 'ls')) {
+
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(Now Listening   ${argresult})
-  } else 
-  if (message.content.startsWith(adminprefix + 'stream')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/idk");
-      message.channel.send(Now Streaming   ${argresult})
-  }
-  if (message.content.startsWith(adminprefix + 'setname')) {
+
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+
+  } else    
+
+    if (message.content.startsWith(adminprefix + 'setname')) {
+
   client.user.setUsername(argresult).then
-      message.channel.send(Changing The Name To ..${argresult} )
-} else
-if (message.content.startsWith(adminprefix + 'setavatar')) {
+
+      message.channel.sendMessage(`**${argresult}** : Done :>`)
+
+  return message.reply("**You Can't Change Your Name ,Only After Two Hours :>**");
+
+  } else
+
+    if (message.content.startsWith(adminprefix + 'setavatar')) {
+
   client.user.setAvatar(argresult);
-    message.channel.send(Changing The Avatar To :${argresult} `);
-}
-});
+
+    message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+
+        } else    
+
+  if (message.content.startsWith(adminprefix + 'st')) {
+
+    client.user.setGame(argresult, "https://www.twitch.tv/mohamedgamal");
+
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+
+  }
+
+    if(message.content === adminprefix + "restart") {
+
+      if (!devs.includes(message.author.id)) return;
+
+          message.channel.send(`:warning:️ **Bot restarting by ${message.author.username}**`);
+
+        console.log("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        console.log(`⚠️ Bot restarting... ⚠️`);
+
+        console.log("===============================================\n\n");
+
+        client.destroy();
+
+        child_process.fork(__dirname + "/bot.js");
+
+        console.log(`Bot Successfully Restarted`);
+
+    }
+
+ 
+
+  });
+
 
 
 client.login(process.env.BOT_TOKEN);
